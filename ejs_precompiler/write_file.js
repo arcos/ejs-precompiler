cb34cc71f78a9825607f488899f86ae4a7b49799
Andrea Costantini <andrea@arcosdev.com>
2011-03-24 07:19:50 -0400

    Comments added, update to html example

--- ejs_precompiler/write_file.js
+++ ejs_precompiler/write_file.js
@@ -1,7 +1,10 @@
 var fs = require('fs'),
     ejs = require('./ejs_server'),
     browser_upgrade_template = require('./panel.browser.upgrade');
-    
+
+//TODO make regular EJS call to .ejs or .html file instead of having a precached template    
+//TODO make the regular EJS call for every .ejs or .html file in a specific directory
+
 var ejsCompile = new EJS.Compiler(browser_upgrade_template.text);
 
 var panel_browser_upgrade = ejsCompile.compile(null, 'panel_browser_upgrade');    
